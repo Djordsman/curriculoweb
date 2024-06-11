3688122a78a35d3ddff5395284568ce9a5f81cde
djordsman <jorgecustodio_89@hotmail.com>
2024-06-10 22:25:50 -0300

    Iniciando novo projeto de web curriculo, inserindo ícones,primeiros ajustes de fonte e dados para contato

---
+++ node_b488f14cf1bfd.tmp.js
@@ -0,0 +1,9 @@
+#header {
+    background-color: rgb(17, 243, 224);
+    margin-top: 50px;
+    margin-bottom: 50px;
+    font-family: "Hubballi", sans-serif;
+    text-transform: uppercase;
+}
+
+h1, h2, h3                   56t
